00c1e1109f07ab2457e29bd470f164fa63e03fcb
Sam Laing <samuelwlaing@gmail.com>
2016-07-26 15:46:25 -0400

    gets all the ip addresses from ip names that it can
     Changes to be committed:
    	modified:   NASA_HTTP_analysis.R

--- NASA_HTTP_analysis.R
+++ NASA_HTTP_analysis.R
@@ -1,15 +1,34 @@
 library(ggplot2)
 library(gdata)
 library(iptools)
+library(rjson)
 
 raw_log <- read.table('../Projects/data/NASA_access_log_Jul95', fill=TRUE, nrows=100)
-log <- subset(raw_log, select=c('V1', 'V4', 'V5', 'V6', 'V7', 'V8'))
 
+log <- subset(raw_log, select=c('V1', 'V4', 'V5', 'V6', 'V7', 'V8'))
+  
 log$V6 <- data.frame(do.call('rbind', strsplit(as.character(log$V6), " ", fixed=TRUE)))
 log$V5 <- gsub(']', '', log$V5)
 log$V4 <- gsub('\\[', '', log$V4)
-
+  
 names(log) <- c('host', 'timestamp', 'timezone', 'request', 'reply', 'reply_size')
 names(log$request) <- c('method', 'path', 'status')
+  
+ips <- character()
+ 
+for(host in as.character(log$host)) {
+  response <- hostname_to_ip(host)
+    
+  if(response == "Not resolved") {
+    host <- gsub('^[^.]*\\.',  '', host)
+    response <- hostname_to_ip(host)
+  }
+  
+  ips <- c(ips, response)
+}
+  
+log$ip_address <- ips
 
-log$ip_address <- hostname_to_ip(as.character(log$host))
+
+
+# log$ip_address <- hostname_to_ip(as.character(log$host))
